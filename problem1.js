a85ad68374cb924ba9ae371070d0339cb492fc0b
AnilKJ1995 <anilrajmontana@hotmail.com>
2023-04-19 19:13:28 +0530

    While loops

---
+++ problem1.js
@@ -0,0 +1,7 @@
+let a = 1; //starting point
+let b = 15; //ending point
+
+while(a<=b){
+  console.log(a);
+  a++;
+}
---
+++ problem2.js
@@ -0,0 +1,10 @@
+let a = 1; //starting point
+let b = 15; //ending point
+
+while(a<=b){
+  if(a%2!=0){
+    console.log(a);
+  }
+  
+  a++;
+}
